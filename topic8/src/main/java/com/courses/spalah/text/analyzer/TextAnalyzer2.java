8152695ecf405cac8cc127be0a18fd1f1079cee4
TOL1990 <olefir.taras@mail.ru>
2016-03-14 17:50:28 +0200

    add count of unique words, and most popular word

--- topic8/src/main/java/com/courses/spalah/text/analyzer/TextAnalyzer2.java
+++ topic8/src/main/java/com/courses/spalah/text/analyzer/TextAnalyzer2.java
@@ -1,6 +1,9 @@
 package com.courses.spalah.text.analyzer;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Тарас on 13.03.2016.
@@ -107,9 +110,9 @@ return null;
         statistic = (
         "Общее количество символов: " +                           getOverSymbolsCount(fileString) +
    "\n"+ "Количество слов: " +                                     getOverWordsCount(fileString) +
-   "\n"+ "Количество предложений: "     +                          getSentencesCount(fileString)
-//        "\n"+ "Количество уникальных слов: "  +                         getUniqueWordsCount(fileString) +
-//       "\n"+ "Самое часто встречающееся слово: " +                     getMostPopularWord(fileString)+
+   "\n"+ "Количество предложений: "     +                          getSentencesCount(fileString)+
+   "\n"+ "Количество уникальных слов: "  +                         getUniqueWordsCount(fileString)+
+   "\n"+ "Самое часто встречающееся слово: " +                     getMostPopularWord(fileString)
 //        "\n"+ "Длинa самого короткого слова: " +                        getLengthOfShortestWord(fileString) +
 //        "\n"+ "Длинa самого длинного слова: " +                         getLengthOfLongestWord(fileString) +
 //        "\n"+ "Сколько раз каждое слово было встреченно в тексте: " +   getCountForEachWordMeetInText(fileString) +
@@ -118,6 +121,60 @@ return null;
 
 
         return statistic;
+    }
+
+    private String getMostPopularWord(String text) {
+        ArrayList<String> mostPopularWords = new ArrayList<>();
+        int meetTimes = 0;
+
+        Map<String, Integer> wordsMap = new HashMap <String, Integer>();
+
+        String [] textArr = text.split("\\s*[,|.|!|?|-|:|;|\\s]+\\s*");
+        for(int i = 0; i < textArr.length; i++)
+        {
+            if(!wordsMap.containsKey(textArr[i])) wordsMap.put(textArr[i], 1);
+            else
+            {
+                int count = (int) wordsMap.get(textArr[i]);
+                count ++;
+                wordsMap.put(textArr[i], count);
+            }
+        }
+        for (Map.Entry<String, Integer> pair : wordsMap.entrySet())
+                if(pair.getValue() > meetTimes) meetTimes = pair.getValue();
+
+
+        for (Map.Entry<String, Integer> pair : wordsMap.entrySet())
+        {
+            if(pair.getValue() == meetTimes) mostPopularWords.add(pair.getKey());
+        }
+        System.out.println(meetTimes + " times");
+        return mostPopularWords.toString();
+    }
+
+    private int getUniqueWordsCount(String text) {
+
+        int uniqueWordsCount = 0;
+
+        Map<String, Integer> wordsMap = new HashMap <String, Integer>();
+        String [] textArr = text.split("\\s*[,|.|!|?|-|:|;|\\s]+\\s*");
+        for(int i = 0; i < textArr.length; i++)
+        {
+            if(!wordsMap.containsKey(textArr[i])) wordsMap.put(textArr[i], 1);
+            else
+            {
+                int count = (int) wordsMap.get(textArr[i]);
+                count ++;
+                wordsMap.put(textArr[i], count);
+            }
+        }
+
+        for (Map.Entry<String, Integer> pair : wordsMap.entrySet())
+        {
+            if(pair.getValue() == 1)uniqueWordsCount++;
+        }
+
+        return uniqueWordsCount;
     }
 
     private int getSentencesCount(String text) {
